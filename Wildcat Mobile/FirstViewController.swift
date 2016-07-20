65a94929c89e6eb66e38107cfbd88908a13d866e
Nicholas Winans <nwinans15@gmail.com>
2016-07-20 16:22:38 -0400

    Initial Commit

---
+++ Wildcat Mobile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wildcat Mobile
+//
+//  Created by Nicholas Winans on 7/20/16.
+//  Copyright © 2016 Centreville HS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Wildcat Mobile/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Wildcat Mobile
+//
+//  Created by Nicholas Winans on 7/20/16.
+//  Copyright © 2016 Centreville HS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
