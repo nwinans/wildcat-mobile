cbe29a4fbc74bd9936ba2e26f6ade70ce87bd068
Nicholas Winans <nwinans15@gmail.com>
2016-08-15 20:30:28 -0400

    Re-add main.storyboard in XCode 7.3.1 as opposed to XCode 8.0 (Beta)

--- Wildcat Mobile/FirstViewController.swift
+++ Wildcat Mobile/FirstViewController.swift
@@ -2,6 +2,10 @@
 //  FirstViewController.swift
 //  Wildcat Mobile
 //
+//	This ViewController handles the announcements page and the NavigationBar items associated with it
+//
+//
+//
 //  Created by Nicholas Winans on 7/20/16.
 //  Copyright © 2016 Centreville HS. All rights reserved.
 //
@@ -10,9 +14,18 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+	@IBAction func plusSchedule(sender: UIBarButtonItem) {
+	
+		print("SUCCESS")
+	
+	}
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 
 	override func didReceiveMemoryWarning() {
---
+++ Wildcat Mobile/mainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  mainTabBarController.swift
+//  Wildcat Mobile
+//
+//	This ViewController is to handle presses 
+//
+//  Created by Nicholas Winans on 7/20/16.
+//  Copyright © 2016 Centreville HS. All rights reserved.
+//
+
+
+class mainTabBarController: UITabBarController {
+
+	
+	
+}
