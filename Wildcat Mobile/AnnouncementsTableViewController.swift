d36cebe32bda0e09a8e3825464b9f2da2a381867
Nicholas Winans <nicholaswinans@nicholass-macintosh.local>
2016-08-16 23:01:30 -0400

    Added more comments to AnnouncementViewController and changed cache policy to allow using a cache Removed commented out optional tableView methods

--- Wildcat Mobile/AnnouncementsTableViewController.swift
+++ Wildcat Mobile/AnnouncementsTableViewController.swift
@@ -2,6 +2,9 @@
 //  AnnouncementsTableViewController.swift
 //  Wildcat Mobile
 //
+//	This ViewController handles the Announcements scene and downloads the JSON form of a database and parses it and then sets tableView with that information
+//	TODO: add check if end date of announcement is before or after current data and include it if it is after the current date
+//
 //  Created by Nicholas Winans on 8/15/16.
 //  Copyright © 2016 Centreville HS. All rights reserved.
 //
@@ -15,8 +18,6 @@ class AnnouncementsTableViewController: UITableViewController {
 	var nameArray:Array<String> = Array<String>()
 	var activityArray: Array<String> = Array<String>()
 	var dateArray: Array<String> = Array<String>()
-	
-	var isRefreshing = false
 
 	//Spreadsheet URL (script converts spreadsheet into JSON for downloading)
 	var defaultSpreadsheetURL = "https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec?id=1ZET4Sf4U3j-8kBuTRVEgk7szsESiwLss6OgaxSLIMik&sheet=Sheet1"
@@ -25,18 +26,25 @@ class AnnouncementsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+		//runs function to populate arrays with data and then eventaully refreshes the table with new data
 		getDataFromURL(defaultSpreadsheetURL)
 		
+		//functions used to auto-resize the height of each row, using both seems redundant but for some reason is required
 		tableView.estimatedRowHeight = 100
 		tableView.rowHeight = UITableViewAutomaticDimension
 		
-		
+		//setup refresh control
 		refreshControl = UIRefreshControl()
+		
+		//set text of refresh control to string
 		refreshControl!.attributedTitle = NSAttributedString(string: "Pull to refresh")
+		
+		//when the user swipes down (and internally calls the ValueChanged function), the refresh function is called
 		refreshControl!.addTarget(self, action: #selector(AnnouncementsTableViewController.refresh(_:)), forControlEvents: UIControlEvents.ValueChanged)
 		
     }
 
+	//unused default function
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -61,17 +69,26 @@ class AnnouncementsTableViewController: UITableViewController {
 		cell.activityLabel.text = activityArray[indexPath.row]
 		cell.dateLabel.text = dateArray[indexPath.row]
 		
-		cell.nameLabel.sizeToFit()
-
         return cell
     }
 	
+	//function to download JSON data from server
 	func getDataFromURL(url: String) {
+		
 		let timeout = 15.0
 		let url = NSURL(string: url)
-		let urlRequest = NSMutableURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: timeout)
+		
+		//setup url request with url, default cache policy, and timeout length
+		let urlRequest = NSMutableURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: timeout)
+		
 		let queue = NSOperationQueue()
+		
+		//actually get the information asynchronously
 		NSURLConnection.sendAsynchronousRequest(urlRequest, queue: queue) { (response: NSURLResponse?, data: NSData?, error: NSError?) in
+			
+			//if the data has length and there was no error, extract the JSON tree from the data
+			//else if the data length is 0 and there was no error, print a message to the console that there was nothing to download at the url
+			//else if there was an error, print the error to the console
 			if data!.length > 0 && error == nil {
 				self.extractJSON(data!)
 			} else if data!.length == 0 && error == nil {
@@ -82,26 +99,36 @@ class AnnouncementsTableViewController: UITableViewController {
 		}
 	}
 	
-	func doTableRefresh() {
-		
-		dispatch_async(dispatch_get_main_queue(), {
-			self.tableView.reloadData()
-			return
-		})
-		
-	}
 	
+	//function to extract json tree from nsdata object and then extract data from the json tree
 	func extractJSON(jsonData: NSData) {
+			//try to parse the json data into an object, json
 			let json: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(jsonData, options: .AllowFragments)
-			
+		
+			//sets table equal to the Sheet1 json object (in this case the whole thing)
 			if let table = json?["Sheet1"] as? NSArray{
-			
+				
+				//loop through all the announcements in the json object, table
 				for var i in 0.stride(to: table.count, by: 1) {
+					
+					//sets announcementObject equal to to the current object in the table array as type NSDictionary
 					if let announcementObject = table[i] as? NSDictionary {
+						
+						//sets announcementApproved equal to the "Approved" json object
 						if let announcementApproved = announcementObject["Approved"] as? String {
+							
+							//sets announcementMessage equal to the announcement object
 							if let announcementMessage = announcementObject["What_is_the_announcement?"] as? String {
+								
+								//sets announcementName equal to the name of the person who is sending the announcement
 								if let announcementName = announcementObject["What_is_your_name?"] as? String {
+									
+									//sets announcementActivity equal to the activity the announcement is related to
 									if let announcementActivity = announcementObject["What_activity_is_this_announcement_related?"] as? String {
+										
+										//check to see if the announcement is approved, or it is empty
+										// if the announcement is empty, it prints a message to the console saying which announcement wasn't approved
+										// if the announcement is approved, it adds all the relevant data into the relevant arrays
 										if announcementApproved.isEmpty {
 											print("The announcement \(announcementMessage) wasn't approved")
 										} else {
@@ -118,66 +145,35 @@ class AnnouncementsTableViewController: UITableViewController {
 				}
 			}
 		
+		//refresh the table with the new information
 		doTableRefresh();
 	}
 	
 	
+	//function to refresh the table on the main queue
+	func doTableRefresh() {
+		
+		dispatch_async(dispatch_get_main_queue(), {
+			self.tableView.reloadData()
+			return
+		})
+		
+	}
+	
+	//storyboard outlet (action) to handle the swipe to refresh feature
 	@IBAction func refresh(sender: UIRefreshControl) {
+		
+		//remove all data from the arrays to prevent duplicate data
 		announcementArray.removeAll()
 		activityArray.removeAll()
 		nameArray.removeAll()
 		dateArray.removeAll()
 		
+		//re-run the getDataFromURL function which is originally called when the view is first loaded
 		getDataFromURL(defaultSpreadsheetURL)
 		
+		
+		//sends information to sender that the table is done refreshing
 		sender.endRefreshing()
 	}
-	
-	
-	
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
