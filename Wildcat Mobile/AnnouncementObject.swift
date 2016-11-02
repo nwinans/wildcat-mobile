62f9b641a8137ad676b784074e18cc28ffc3625f
Nick Winans <nwinans15@gmail.com>
2016-11-02 12:37:54 -0400

    Create AnnouncementObject to hold announcements

---
+++ Wildcat Mobile/AnnouncementObject.swift
@@ -0,0 +1,62 @@
+//
+// AnnouncementObject.swift
+// Wildcat Mobile
+// 
+// Created by Nicholas Winans on 11/2/16.
+// Copyright © 2016 Centreville HS. All rights reserved.
+
+import UIKit
+
+class AnnouncementObject: NSObject NSCoding {
+		var activity: String
+        var announcement: String
+        var date: String
+        var name: String
+        
+        static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+		
+		static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("announcements")
+		
+		struct announcements {
+			static let activity = "activity"
+			static let announcement = "announcement"
+			static let date = "date"
+			static let name = "name"
+		}
+		
+		func encodeWithCoder(aCoder: NSCoder) {
+			aCoder.encodeObject(activity, forKey: announcements.activity)
+			aCoder.encodeObject(announcement, forKey: announcements.announcement)
+			aCoder.encodeObject(date, forKey: announcements.date)
+			aCoder.encodeObject(name, forKey: announcements.name)
+		}
+		
+		required convenience init?(coder aDecoder: NSCoder) {
+			let activity = aDecoder.decodeObjectForKey(announcements.activity) as! String
+			let annoucement = aDecoder.decodeObjectForKey(announcements.announcement) as! String
+			let date = aDecoder.decodeObjectForKey(announcements.date) as! String
+			let name = aDecoder.decodeObjectForKey(announcements.name) as! String
+			
+			self.init(activity: activity, announcement: announcement, date: date, name: name)
+		}
+		
+		init?(activity: activity, announcement: announcement, date: date, name: name) {
+			self.activity = activity
+			self.announcement = announcement
+			self.date = date
+			self.name = name
+			
+			super.init()
+			
+			if activity.isEmpty || announcement.isEmpty || date.isEmpty || name.isEmpty {
+				return nil
+			}
+		}
+		
+		override init(){
+			self.activity = "Error"
+			self.announcement = "There was an error downloading this announcement, please try again later."
+			self.date = "1/1/17"
+			self.name = "Error"
+		}
+}
