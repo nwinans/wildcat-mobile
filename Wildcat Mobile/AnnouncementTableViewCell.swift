a05088d7c6ac01cb9493cb7b07c5014b7c0e6d36
Nick Winans <nwinans15@gmail.com>
2016-11-14 13:39:19 -0500

    Commented Announcement Cell
    Also removed unused class (setSelected)

--- Wildcat Mobile/AnnouncementTableViewCell.swift
+++ Wildcat Mobile/AnnouncementTableViewCell.swift
@@ -6,47 +6,47 @@
 //  Copyright © 2016 Centreville HS. All rights reserved.
 //
 
+//import the UIKit as we use UILabels and UITableViewCell in this class
 import UIKit
 
 class AnnouncementTableViewCell: UITableViewCell {
 
+	//get the labels from the storyboard
 	@IBOutlet weak var announcementLabel: UILabel!
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var activityLabel: UILabel!
 	@IBOutlet weak var dateLabel: UILabel!
 	
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        self.announcementLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-        self.nameLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
-        self.dateLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
+    	override func awakeFromNib() {
+        	super.awakeFromNib()
+        	//set font of announcement label to body text and the name + date to footnote objects
+        	self.announcementLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        	self.nameLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
+        	self.dateLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
         
-        let activityDescriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle(UIFontTextStyleHeadline)
+		//create a UIFontDescriptor that holds the aspects of the headline text style and store it a variable in activity descriptor
+        	let activityDescriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle(UIFontTextStyleHeadline)
         
-        let boldTitleDescriptor = activityDescriptor.fontDescriptorWithSymbolicTraits(.TraitBold)
+		//create a variable called boldTitle that makes the activityDescriptor bold
+        	let boldTitleDescriptor = activityDescriptor.fontDescriptorWithSymbolicTraits(.TraitBold)
         
-        self.activityLabel.font = UIFont(descriptor: boldTitleDescriptor!, size: 0)
+		//set the activityLabel font to boldTitleDescriptor and a size that is scaled 1.25x the default font size (so the title and body aren't the same size
+        	self.activityLabel.font = UIFont(descriptor: boldTitleDescriptor!, size: 0)
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AnnouncementTableViewCell.didChangePreferredContentSize), name: UIContentSizeCategoryDidChangeNotification, object: nil)
-		
-        
-    }
+		//create a observer that listens for font size changes in Settings > Accessibility (this makes the font instantly scale up when the user return to the app after changing font sizes
+        	NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AnnouncementTableViewCell.didChangePreferredContentSize), name: UIContentSizeCategoryDidChangeNotification, object: nil)        
+    	}
     
-    func didChangePreferredContentSize() {
-        self.announcementLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
-        self.nameLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
-        let activityDescriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle(UIFontTextStyleHeadline)
+    	func didChangePreferredContentSize() {
+	   
+		//repeat of above that re-sets fonts to new sizes when the user changes their font size mid app session 
+        	self.announcementLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        	self.nameLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
+        	let activityDescriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle(UIFontTextStyleHeadline)
         
-        let boldTitleDescriptor = activityDescriptor.fontDescriptorWithSymbolicTraits(.TraitBold)
+       		let boldTitleDescriptor = activityDescriptor.fontDescriptorWithSymbolicTraits(.TraitBold)
         
-        self.activityLabel.font = UIFont(descriptor: boldTitleDescriptor!, size: 0)
-        self.dateLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+        	self.activityLabel.font = UIFont(descriptor: boldTitleDescriptor!, size: 0)
+        	self.dateLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleFootnote)
+	}
 }
