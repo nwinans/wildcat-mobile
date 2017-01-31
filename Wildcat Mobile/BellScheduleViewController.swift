6bf15c5516d82b7e48c6dac5ed6633eff581838a
Nick Winans <nwinans15@gmail.com>
2017-01-30 22:49:07 -0500

    create bell schedule tableview controller

---
+++ Wildcat Mobile/BellScheduleViewController.swift
@@ -0,0 +1,13 @@
+//
+// BellScheduleViewController.swift
+// Wildcat Mobile
+//
+// Created by Nick Winans on 1/30/2017
+// Copyright © 2017 Centreville HS. All rights reserved.
+//
+
+import UIKit
+
+class BellScheduleViewController: UITableViewController {
+
+}
