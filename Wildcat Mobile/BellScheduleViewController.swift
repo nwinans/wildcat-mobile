d5e4d13a040c39d615cbd5de048b6e87ef8af153
rithikakormath <rithika.kormath@gmail.com>
2017-01-31 09:46:52 -0500

    magic

--- Wildcat Mobile/BellScheduleViewController.swift
+++ Wildcat Mobile/BellScheduleViewController.swift
@@ -21,7 +21,13 @@ class BellScheduleViewController: UITableViewController {
     let regBlock5 = "7th period"
     let regBlock5T = "2:08 - 2:55"
     let regBlockL1 = "1st Lunch"
-	let regBlockL1T = "2st Lunch times go here"
+    let regBlockL1T = "11:52 - 12:20"
+    let regBlockL2 = "2nd Lunch"
+    let regBlockL2T = "12:25 - 12:54"
+    let regBlockL3 = "3rd Lunch"
+    let regBlockL3T = "12:59 - 1:28"
+    let regBlockL4 = "4th Lunch"
+    let regBlockL4T = "1:33 - 2:01"
   
     //2 hour early release block schedule, 4 blocks because no plus (and 4 lunches)
     let ereBlock1 = "1st/2nd period"
@@ -33,6 +39,14 @@ class BellScheduleViewController: UITableViewController {
     let ereBlock3T = "10:11 - 10:47"
     let ereBlock4 = "5th/6th period"
     let ereBlock4T = "10:52 - 12:55"
+    let ereBlockL1 = "1st Lunch"
+    let ereBlockL1T = "10:47 - 11:16"
+    let ereBlockL2 = "2nd Lunch"
+    let ereBlockL2T = "11:21 - 11:49"
+    let ereBlockL3 = "3rd Lunch"
+    let ereBlockL3T = "11:54 - 12:23"
+    let ereBlockL4 = "4th Lunch"
+    let ereBlockL4T = "12:28 - 12:55"
 	
     //2 hour delay schedule, 4 blocks becuase no plus (and 4 lunches)
     let delBlock1 = "1st/2nd period"
@@ -43,7 +57,15 @@ class BellScheduleViewController: UITableViewController {
     let delBlock3T = "12:20 - 2:24"
     let delBlock4 = "7th period"
     let delBlock4T = "2:30 - 2:55"
-		
+    let delBlockL1 = "1st Lunch"
+    let delBlockL1T = "12:14 - 12:43"
+    let delBlockL2 = "2nd Lunch"
+    let delBlockL2T = "12:48 - 1:17"
+    let delBlockL3 = "3rd Lunch"
+    let delBlockL3T = "1:22 - 1:50"
+    let delBlockL4 = "4th Lunch"
+    let delBlockL4T = "1:55 - 2:24"
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.estimatedRowHeight = 100
@@ -63,7 +85,7 @@ class BellScheduleViewController: UITableViewController {
     }
 	
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 4
     }
 		
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
